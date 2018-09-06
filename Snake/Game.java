6117a477db259dd8098f1ad765b7392053775a36
defCoding <39608887+defCoding@users.noreply.github.com>
2018-09-06 11:31:09 -0400

    Snake
    Created a basic version of Snake using DrJava for a homework assignment and then finished it.

---
+++ Snake/Game.java
@@ -0,0 +1,138 @@
+package Homework_10;
+
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import Homework_10.Snake.*;
+import Homework_10.Segment.*;
+import Homework_10.Grid.*;
+import Homework_10.Food.*;
+import Homework_10.DrawUtil.*;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+/**
+ * This class models the frame in which the game will be played on.
+ * */
+public class Game extends JFrame implements KeyListener, ActionListener {
+  public static final int FRAMESIZE = 600;
+  private static Snake snake;
+  private static Grid grid;
+  private static boolean isFood;
+  private static Food food;
+  private static DrawUtil drawer;
+  private static Timer timer;
+  /**
+   * This constructor creates the frame and makes it visible.
+   * */
+  public Game() {
+    final int SNAKESIZE = 10;
+    final int SEGSIZE = 10;
+    Game.isFood = true;
+    Game.grid = new Grid(Game.FRAMESIZE, SEGSIZE);
+    
+    int gridLength = Game.grid.getGrid().length;
+    Random generator = new Random();
+    Game.food = new Food(Game.grid.getGrid()[generator.nextInt(gridLength)][generator.nextInt(gridLength)]);
+    
+    Game.snake = new Snake(SNAKESIZE, SEGSIZE, Game.grid);
+    
+    Game.drawer = new DrawUtil(Game.snake, Game.food);
+    setSize(Game.FRAMESIZE + 15, Game.FRAMESIZE + 40);
+    setVisible(true);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    Game.timer = new Timer(100, this);
+  }
+  /**
+   * Changes the velocity depending on keypress.
+   * @param e The key event.
+   * */
+  public void keyPressed(KeyEvent e) {
+    Segment segment = Game.snake.getSegments().get(0);
+    if (e.getKeyCode() == 38) {
+      snake.startMoving();
+      snake.changeYVel(-1);
+      snake.changeXVel(0);
+    }
+    else if (e.getKeyCode() == 40) {
+      snake.startMoving();
+      snake.changeYVel(1);
+      snake.changeXVel(0);
+    }
+    else if (e.getKeyCode() == 37) {
+      snake.startMoving();
+      snake.changeXVel(-1);
+      snake.changeYVel(0);
+    }
+    else if (e.getKeyCode() == 39) {
+      snake.startMoving();
+      snake.changeXVel(1);
+      snake.changeYVel(0);
+    }
+    else if (e.getKeyCode() == 80) {
+      Game.timer.stop();
+    }
+    else if (e.getKeyCode() == 79) {
+      Game.timer.start();
+    }
+  }
+  public void keyReleased(KeyEvent e) {}
+  public void keyTyped(KeyEvent e) {}
+  
+  /**
+   * This moves the snake when an action has occured.
+   * @param e The action event.
+   * */
+  public void actionPerformed(ActionEvent e) {
+    if (Game.snake.isMoving()) {
+      try {
+        Game.snake.move();
+      } catch (ArrayIndexOutOfBoundsException exc) {
+        Game.drawer.setGameOver(true);
+        Game.timer.stop();
+      }
+    }
+    if (Game.snake.getSegments().get(0).getPoint().equals(Game.food.getPoint())) {
+      Game.snake.addSegment();
+      Game.isFood = false;
+    }
+    if (!this.isFood()) {
+      this.makeFood();
+      Game.isFood = true;
+    }
+    if (!Game.snake.checkSnakeHead()) {
+      Game.drawer.setGameOver(true);
+      Game.timer.stop();
+    }
+    this.repaint();
+  }
+  /**
+   * This method creates food on the map.
+   * */
+  public void makeFood() {
+    int gridLength = Game.grid.getGrid().length;
+    Random generator = new Random();
+    Game.food.move(Game.grid.getGrid()[generator.nextInt(gridLength)][generator.nextInt(gridLength)]);
+  }
+  /**
+   * This method checks if there is already food.
+   * @return True if there is food, false otherwise.
+   * */
+  public boolean isFood() {
+    return Game.isFood;
+  }
+  /**
+   * This main method runs the game.
+   * @param args Not used.
+   * */
+  public static void main(String[] args) {
+    Game game = new Game();
+    game.add(Game.drawer);
+    game.addKeyListener(game);
+    Game.timer.start();
+  }
+}
---
+++ Snake/Snake/Snake.java
@@ -0,0 +1,146 @@
+package Homework_10.Snake;
+
+import Homework_10.Segment.*;
+import Homework_10.Grid.*;
+import java.util.ArrayList;
+import javax.swing.JComponent;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Point;
+
+/**
+ * This class models a snake for the game.
+ * */
+public class Snake {
+  private ArrayList<Segment> segments = new ArrayList<Segment>();
+  private int xVel, yVel, firstIndex, secondIndex, segSize;
+  private boolean isMoving;
+  private Grid grid;
+  /** This is the constructor for the snake.
+    * @param size The initial starting size.
+    * @param segSize The size of the segments.
+    * @param grid The grid the snake is on.
+    * */
+  public Snake(int size, int segSize, Grid grid) {
+    this.grid = grid;
+    this.segSize = segSize;
+    int gridLength = grid.getGrid().length;
+    Point startingPoint = grid.getGrid()[gridLength / 2][gridLength / 2];
+    this.firstIndex = gridLength / 2;
+    this.secondIndex = gridLength/ 2;
+    Segment head = new Segment(startingPoint, Color.RED, segSize);
+    this.xVel = 0;
+    this.yVel = 0;
+    this.isMoving = false;
+    segments.add(head);
+    for (int i = 0; i < size; i++) {
+      Segment lastSegment = segments.get(segments.size() - 1);
+      Segment newSegment = new Segment(new Point((int) lastSegment.getPoint().getX(), 
+                                                 (int) lastSegment.getPoint().getY() + lastSegment.getSize()), 
+                                       Color.BLACK, segSize);
+      segments.add(newSegment);
+    }
+  }
+  /**
+   * Gets the segments ArrayList.
+   * @return The ArrayList.
+   * */
+  public ArrayList<Segment> getSegments() {
+    return this.segments;
+  }
+  /**
+   * Adds a segment to Segments.
+   * */
+  public void addSegment() {
+    Segment lastSegment = segments.get(segments.size() - 1);
+    try {
+      if (this.yVel != 0) {
+        Segment newSegment = new Segment(new Point((int) lastSegment.getPoint().getX(), 
+                                                   (int) lastSegment.getPoint().getY() + lastSegment.getSize() * -this.yVel), 
+                                         Color.BLACK, segSize);
+      } else {
+        Segment newSegment = new Segment(new Point((int) lastSegment.getPoint().getX() + lastSegment.getSize() * -this.xVel, 
+                                                   (int) lastSegment.getPoint().getY()), 
+                                         Color.BLACK, segSize);
+        segments.add(newSegment);
+      }
+    } catch (ArrayIndexOutOfBoundsException e) {
+      if (this.yVel != 0) {
+        Segment newSegment = new Segment(new Point((int) lastSegment.getPoint().getX() + lastSegment.getSize() * -this.xVel, 
+                                                   (int) lastSegment.getPoint().getY()), 
+                                         Color.BLACK, segSize);
+        segments.add(newSegment);
+      } else {
+        Segment newSegment = new Segment(new Point((int) lastSegment.getPoint().getX(), 
+                                                   (int) lastSegment.getPoint().getY() + lastSegment.getSize() * -this.yVel), 
+                                         Color.BLACK, segSize);
+        segments.add(newSegment);
+      }
+    }
+    
+  }
+  /**
+   * Changes the x velocity.
+   * @param vel The new velocity.
+   * */
+  public void changeXVel(int vel) {
+    this.xVel = vel;
+  }
+  /**
+   * Changes the y velocity.
+   * @param vel The new velocity.
+   * */
+  public void changeYVel(int vel) {
+    this.yVel = vel;
+  }
+  /**
+   * Allows the snake to move.
+   * */
+  public void startMoving() {
+    this.isMoving = true;
+  }
+  /**
+   * Checks if the snake is moving.
+   * @return True if snake is moving.
+   * */
+  public boolean isMoving() {
+    return this.isMoving;
+  }
+  /** 
+   * Checks if head is legal.
+   * @return True if head is legal, false otherwise.
+   * */
+  public boolean checkSnakeHead() {
+    boolean legal = true;
+    for (int i = 1; i < this.segments.size(); i++) {
+      if (this.segments.get(0).equals(this.segments.get(i))) {
+        legal = false;
+        break;
+      }
+    }
+    return legal;
+  }
+  /**
+   * This method paints the snake.
+   * @param g The graphics used.
+   * */
+  public void draw(Graphics g) {
+    for (Segment seg : segments) {
+      seg.draw(g);
+    }
+  }
+  /** 
+   * This method moves the snake.
+   * */
+  public void move() {
+    Segment head = segments.get(0);
+    Segment newHead = segments.remove(segments.size() - 1); // Removes last segment and saves it.
+    newHead.move(head.getPoint());
+    segments.add(1, newHead);
+    
+    this.firstIndex = this.firstIndex + yVel;
+    this.secondIndex = this.secondIndex + xVel;
+    head.move(this.grid.getGrid()[this.firstIndex][this.secondIndex]);
+  }
+}
+
